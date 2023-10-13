b5dc2aa8cdf2c90f74df0828a97add2e61e38c8f
faisbisa <faisalkurnis12@gmail.com>
2023-10-13 08:42:42 +0700

    mstQA

---
+++ Scripts/MSTQA/Addtgodo4/Script1697113264850.groovy
@@ -0,0 +1,57 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('mobileApp/mst-qa-0.0.1.apk', false)
+
+Mobile.delay(3)
+
+Mobile.tap(findTestObject('Addtodo/android.widget.TextView - Add Todo'), 0)
+
+Mobile.delay(3)
+
+Mobile.setText(findTestObject('Addtodo/android.widget.EditText - Input Name'), 'MST QA Test 2', 0)
+
+Mobile.delay(3)
+
+Mobile.tap(findTestObject('Addtodo/android.widget.TextView -'), 0)
+
+Mobile.delay(3)
+
+Mobile.tap(findTestObject('Addtodo2/android.widget.TextView - 21'), 0)
+
+Mobile.delay(3)
+
+Mobile.tap(findTestObject('Addtodo/android.widget.TextView - OK'), 0)
+
+Mobile.delay(5)
+
+Mobile.tapAndHold(findTestObject('Addtodo/android.widget.ImageView'), 0, 1)
+
+Mobile.delay(3)
+
+Mobile.doubleTap(findTestObject('Addtodo2/android.widget.TextView - Other'), 0)
+
+Mobile.delay(3)
+
+Mobile.setText(findTestObject('Addtodo/android.widget.EditText - Input Description'), 'Testing', 0)
+
+Mobile.tap(findTestObject('Addtodo/android.widget.TextView - Add Todo'), 0)
+
+Mobile.delay(3)
+
---
+++ Test Suites/MST.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>MST</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>12b66c57-ca49-4435-8b48-711b19ffb932</testSuiteGuid>
+   <testCaseLink>
+      <guid>c3270589-ff95-45da-afb4-fd93c9e68ffd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/MSTQA/Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
